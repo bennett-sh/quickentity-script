ba60417d03f9a5f7009fafbaeec1d6a93441b6b6
Bennett <110846042+bennett-sh@users.noreply.github.com>
2023-05-03 20:07:22 +0200

    bug fix

--- src/patches/entity/events.ts
+++ src/patches/entity/events.ts
@@ -44,7 +44,7 @@ Entity.prototype.addOnGameStartListener = function(outputs) {
   let listener = new Entity(this.patch, id)
   if(!id) {
     listener = createQNSEventListeners(this)
-    this.__constants.EVENT_LISTENERS = listener.id
+    this.patch.__constants.EVENT_LISTENERS = listener.id
   }
   Object.entries(outputs).forEach(([k, v]) => listener.addEvent({ when: 'OnGameStart', do: k, on: v }))
   return listener
