60a0e7a836463b9901838bae937864e6902cab98
Bennett <110846042+bennett-sh@users.noreply.github.com>
2023-05-10 18:33:06 +0200

    expose class

--- src/lib.ts
+++ src/lib.ts
@@ -2,6 +2,7 @@
 export * as entities from './utils/entities.js'
 export * from './patches/PatchActions.js'
 export * as hash from './utils/hash.js'
+export * from './patches/QNPatch.js'
 export * from './patches/patch.js'
 export * from './CommonPaths.js'
 export * from './utils/paths.js'
